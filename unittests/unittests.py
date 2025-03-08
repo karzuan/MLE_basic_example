9ae62783af20e75358650c78d3edcfecce8eb014
karzuan <kutskonstantin@yandex.com>
2025-03-08 16:24:16 +0500

    Worked on unit tests, applied for data load and train steps. Hide lighting logs from Git

--- unittests/unittests.py
+++ unittests/unittests.py
@@ -3,31 +3,41 @@ import pandas as pd
 import os
 import sys
 import json
+import torch
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(current_dir)
+#print('root dir', ROOT_DIR)
 sys.path.append(os.path.dirname(ROOT_DIR))
-CONF_FILE = os.getenv('CONF_PATH')
+# CONF_FILE = os.getenv('CONF_PATH')
+CONF_FILE = ROOT_DIR + '/settings.json'
+#CONF_FILE = "../settings.json"
 
-from training.train import DataProcessor, Training 
+from training.train import DataProcessor
+from training.train import Training
 
+# print(f"Current working directory: {os.getcwd()}")
 
 class TestDataProcessor(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         with open(CONF_FILE, "r") as file:
             conf = json.load(file)
-        cls.data_dir = conf['general']['data_dir']
+        cls.data_dir = ROOT_DIR + '/' + conf['general']['data_dir']
+        print(f"Data directory: {cls.data_dir}")
         cls.train_path = os.path.join(cls.data_dir, conf['train']['table_name'])
 
     def test_data_extraction(self):
         dp = DataProcessor()
+        print(f"Train directory: {self.train_path}")
         df = dp.data_extraction(self.train_path)
+        print(f"Dataframe shape: {df.shape}")
         self.assertIsInstance(df, pd.DataFrame)
 
     def test_prepare_data(self):
         dp = DataProcessor()
-        df = dp.prepare_data(100)
-        self.assertEqual(df.shape[0], 100)
+        df = dp.prepare_data()
+        self.assertEqual(df.shape[0], 120)
 
 
 class TestTraining(unittest.TestCase):
@@ -35,12 +45,14 @@ class TestTraining(unittest.TestCase):
         tr = Training()
         # assume you have some prepared data
         X_train = pd.DataFrame({
-            'x1': [1, 0, 1, 0],
-            'x2': [1, 1, 0, 0]
+            'sepal length(cm)': [4.6, 5.7, 6.4, 4.8],
+            'sepal width(cm)': [3.5, 4.4, 3.3, 2.2],
+            'petal length(cm)': [1.0, 1.5, 4.4, 1.5],
+            'petal width(cm)': [0.3, 0.4, 1.3, 0.2]
         })
-        y_train = pd.Series([0, 1, 1, 0])
+        y_train = pd.Series([0, 0, 1, 0])
         tr.train(X_train, y_train)
-        self.assertIsNotNone(tr.model.tree_)
+        self.assertIsInstance(tr.model, torch.nn.Module)
 
 
 if __name__ == '__main__':
